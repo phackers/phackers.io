b7954f0fb346c25ac1587f05ebc05878d2f434fa
Arman Jon Villalobos <me@armanjon.com>
2015-06-22 22:31:09 +0800

    configure slack integration

--- source/assets/javascripts/slack.js
+++ source/assets/javascripts/slack.js
@@ -1,33 +1,14 @@
-// var invite_button = $('.invite-button');
-
-
-// function invite_user(email, time, token) {
-//   var url = "https://teamgalit.slack.com/api/users.admin.invite?t=" + time +"&email="+ email +"&token="+ token + "&set_active=true";
-//   $.ajax({
-//     type: "POST",
-//     url: url,
-//     success: function() { alert("success!"); }
-//   });
-// }
-
-// invite_button.on("click", invite_user());
-
 var inviteButton = $('.invite-button');
-inviteButton.on('click', function() {
-  var email = encodeURIComponent($('.invite_email').val());
-  var time = (new Date).getTime();
-  var token = "xoxs-6691027749-6690822983-6690897668-1e8900f98c";
-  var url = "https://teamgalit.slack.com/api/users.admin.invite?t=" + time +"&email="+ email +"&token="+ token + "&set_active=true";
-  // $.ajax({
-  //   type: "POST",
-  //   url: url,
-  //   success: function() { alert("check your email!"); }
-  // });
 
-  $.post(url, function() {
-  }).done(function() {
-    alert("success");
-  }).fail(function(error) {
-    alert(error);
+inviteButton.on('click', function() {
+  var email = encodeURIComponent($('.invite-email').val());
+  var time = (new Date).getTime();
+  var token = "xoxp-3359469240-3368537646-3525848797-5d232a";
+  var subdomain = "phackers";
+  var url = "https://" + subdomain + ".slack.com/api/users.admin.invite?t=" + time +"&email="+ email +"&token="+ token + "&set_active=true";
+  $.ajax({
+    type: "POST",
+    url: url,
+    success: function() { alert("check your email!"); }
   });
 });
