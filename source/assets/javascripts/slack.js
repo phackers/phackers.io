9cb97e984a477a06d079443e8db77ff03ee65530
Arman Jon Villalobos <me@armanjon.com>
2015-06-23 16:09:03 +0800

    added api call to to get slack current members

--- source/assets/javascripts/slack.js
+++ source/assets/javascripts/slack.js
@@ -1,6 +1,7 @@
 (function() {
 
   var inviteButton = $('.invite-button');
+  var token = "xoxp-3359469240-3368537646-3525848797-5d232a";
   var subdomain = "phackers";
   var currentUsersTotal = $('.total-users');
 
